4933cdd2c57bfca2a8db8c8c949d12a38ee0a116
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-18 01:34:31 +0100

    created the join.htnl page

---
+++ chamber/scripts/join.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const modalButtons = document.querySelectorAll(".open-modal-btn");
+    const closeButtons = document.querySelectorAll(".close-modal-btn");
+    const modals = document.querySelectorAll(".membership-modal");
+
+    // Open Modal Action
+    modalButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const modalId = button.getAttribute("data-modal");
+            const targetModal = document.getElementById(modalId);
+            if (targetModal) {
+                targetModal.showModal(); 
+            }
+        });
+    });
+
+    // Close Modal Action via Button Click
+    closeButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            const openModal = e.target.closest("dialog");
+            if (openModal) {
+                openModal.close();
+            }
+        });
+    });
+
+    // Close Modal Action via Clicking Outside content window frame boundary zones
+    modals.forEach(modal => {
+        modal.addEventListener("click", (e) => {
+            const dialogDimensions = modal.getBoundingClientRect();
+            if (
+                e.clientX < dialogDimensions.left ||
+                e.clientX > dialogDimensions.right ||
+                e.clientY < dialogDimensions.top ||
+                e.clientY > dialogDimensions.bottom
+            ) {
+                modal.close();
+            }
+        });
+    });
+});
